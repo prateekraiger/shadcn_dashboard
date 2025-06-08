25058786f59fbf3d588929b7568abb7401ba5426
Pratik <prateekraiger123@gmail.com>
2025-06-08 19:08:25 +0530

    added theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -10,7 +12,11 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
   return (
     <nav className="flex p-4 items-center justify-between ">
       {/*left  */}
@@ -18,18 +24,19 @@ const Navbar = () => {
       {/* right */}
       <div className="flex items-center gap-4">
         <Link href="/">DashBoard</Link>
-        <Moon className="cursor-pointer" />
+
+        {/* user menu */}
 
         {/* theme menu */}
-        <Sun className="cursor-pointer" />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" size="icon">
               <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <Moon className="h-[1.2rem] w-[1.2rem] absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
               <span className="sr-only">Toggle theme</span>
             </Button>
           </DropdownMenuTrigger>
+
           <DropdownMenuContent align="end">
             <DropdownMenuItem onClick={() => setTheme("light")}>
               Light
