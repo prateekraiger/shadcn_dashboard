57f1754061b1dc10f1a470e918cc373caba1ddcb
Pratik <prateekraiger123@gmail.com>
2025-06-09 11:50:56 +0530

    updated charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols=2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+    </div>
+  );
 };
 
 export default HomePage;
