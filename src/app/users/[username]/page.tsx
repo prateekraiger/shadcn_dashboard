03f5daa022154076e2726709e87469811b569975
Pratik <prateekraiger123@gmail.com>
2025-06-09 20:29:49 +0530

    updated user page

---
+++ src/app/users/[username]/page.tsx
@@ -0,0 +1,168 @@
+import CardList from "@/components/CartList";
+import { Badge } from "@/components/ui/badge";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Progress } from "@/components/ui/progress";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
+
+const SingleUserPage = () => {
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/users">Users</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Pratik</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div className="mt-4 flex flex-col xl:flex-row gap-8 ">
+        {/* left */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* user badges container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Badges</h1>
+            <div className="flex gap-4 mt-4">
+              <HoverCard>
+                <HoverCardTrigger>
+                  <BadgeCheck
+                    size={36}
+                    className="rounded-full bg-blue-500/30 border-1 border-blue-500/30  p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bolt mb-2">Verified User</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This User has been verified by the Admin.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Shield
+                    size={36}
+                    className="rounded-full bg-green-800/30 border-1 border-green-800/30  p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bolt mb-2">Admin</h1>
+                  <p className="text-sm text-muted-foreground">
+                    Admin users have access to all features and can manage
+                    users.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Candy
+                    size={36}
+                    className="rounded-full bg-blue-500/30 border-1 border-blue-500/30  p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bolt mb-2">Awarded</h1>
+                  <p className="text-sm text-muted-foreground">
+                    This User has been awarded for their contribution.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Citrus
+                    size={36}
+                    className="rounded-full bg-orange-500/30 border-1 border-orange-500/30  p-2"
+                  />
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <h1 className="font-bolt mb-2"> Popular </h1>
+                  <p className="text-sm text-muted-foreground">
+                    This User has been popular in the community.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+          </div>
+
+          {/* info badges container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <h1 className="text-xl font-semibold">User Information</h1>
+
+            <div className="space-y-4 mt-4">
+              <div className="flex flex-col gap-2 mb-8">
+                <p className="text-sm text-muted-foreground">
+                  Profile Completion
+                </p>
+                <Progress value={66} />
+              </div>
+              <div className="flex items-center gap-2">
+                <span className="font-bolt">Username:</span>
+                <span>pratik.dev</span>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="font-bolt">Email:</span>
+                <span>pratik.dev@gmail.com</span>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="font-bolt">Phone:</span>
+                <span>+91 1234567879</span>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="font-bolt">Location:</span>
+                <span>Earth</span>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <span className="font-bolt">Role:</span>
+                <Badge>Admin</Badge>
+              </div>
+              <p className="text-sm text-muted-foreground mt-4">
+                Joined On 2025.06.06
+              </p>
+            </div>
+          </div>
+
+          {/* cardlist badges container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transactions" />
+          </div>
+        </div>
+
+        {/* right */}
+        <div className="w-full xl:w-2/3 space-y-6">
+          {/* user card container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">UserCard</div>
+
+          {/* chart container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleUserPage;
